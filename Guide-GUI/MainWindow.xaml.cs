1b7aba6fb5f63e88e7321135b3ae486537ec6d84
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-11-03 04:30:32 +0100

    Enough of a skeleton to switch in the correct page as needed.

--- Guide-GUI/MainWindow.xaml.cs
+++ Guide-GUI/MainWindow.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using DrainLib;
+using DrainLib.Engines;
+using Henke37.DebugHelp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,14 +16,69 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Page = System.Windows.Controls.Page;
 
 namespace Guide_GUI {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window {
+
+		private ScummVMConnector connector;
+		private Timer updateTimer;
+
+		private BaseEngineAccessor engine;
+
 		public MainWindow() {
 			InitializeComponent();
+
+			connector = new ScummVMConnector();
+
+			UpdateState();
+
+			updateTimer = new Timer(100);
+			updateTimer.Elapsed += UpdateTimer_Elapsed;
+			updateTimer.Start();
+		}
+
+		private void UpdateTimer_Elapsed(object sender, ElapsedEventArgs e) {
+			this.Dispatcher.Invoke(UpdateState);
+		}
+
+		private void UpdateState() {
+			if(!connector.Connected) {
+				try {
+					connector.Connect();
+				} catch(IncompleteReadException) {
+				} catch(ProcessNotFoundException) {
+					ForcePage(typeof(WaitConnect));
+					return;
+				}
+			}
+
+			if(engine!=null && engine.IsActiveEngine) {
+				return;
+			}
+			try { 
+				engine = connector.GetEngine();
+			} catch(IncompleteReadException) {
+			}
+
+			if(engine==null) {
+				ForcePage(typeof(WaitSelectGame));
+				return;
+			}
+		}
+
+		private void ForcePage(Type type) {
+			if(Content != null && Content.GetType() == type) return;
+			var p = (Page)type.InvokeMember("", System.Reflection.BindingFlags.CreateInstance, null, null, null, null);
+
+			Content = p;
+		}
+
+		private void NavigationWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+			updateTimer.Stop();
 		}
 	}
 }
